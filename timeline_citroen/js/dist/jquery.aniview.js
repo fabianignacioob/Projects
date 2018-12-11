abe9be0db0e1a0c446962e9976ddd7c880c9c81c
fabianignacioob <fabianignacio.ob@gmail.com>
2018-12-11 15:43:38 -0300

    Timeline Citroen

---
+++ timeline_citroen/js/dist/jquery.aniview.js
@@ -0,0 +1,19 @@
+(function($) {
+    $(document).ready(function() {
+        $.jScrollability([
+            {
+                'selector': '.slide-in-demo',
+                'start': 'parent',
+                'end': 'parent',
+                'fn': {
+                    'left': {
+                        'start': 100,
+                        'end': 0,
+                        'unit': '%'
+                    }
+                }
+            }
+        ]);
+    });
+});
+            
